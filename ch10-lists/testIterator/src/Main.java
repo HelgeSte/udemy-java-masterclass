f2b44c25da85a044075a5bfbc65b55285be312d4
Helge Stegemoen <hstegemo@hotmail.com>
2024-04-29 22:43:34 +0200

    Added testIterator project

--- ch10-lists/testIterator/src/Main.java
+++ ch10-lists/testIterator/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<String> list = new LinkedList<>();
+        String homer = "Homer";
+        String bart = "Bart";
+        String lisa = "Lisa";
+        String marge = "Marge";
+        list.add(homer);
+        list.add(bart);
+        list.add(lisa);
+        list.add(marge);
+        PrintItinerary3(list);
+    }
+
+    public static void PrintItinerary3(LinkedList<String> list) {
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator(1);
+        while (iterator.hasNext()){
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
     }
 }
